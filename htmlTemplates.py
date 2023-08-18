199d1a78ba66f4b55c4b45623691bca08cd5fd3b
evieran <124214396+evieran@users.noreply.github.com>
2023-08-18 04:13:24 +0300

    Add files via upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/xzkhzgk/0-2-1-copy.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://i.ibb.co/RSBYhT8/Beth-Emily-21-Animiation-Pixar-looking-girl-19-25-who-works-f-2087ab4f-a6b6-4885-a338-5b9f7c4a0daa.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
